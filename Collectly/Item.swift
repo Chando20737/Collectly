f992856e2db0b8ef49ac68e6ffee5f07ca3948d1
Eric Chandonnet <chando20@gmail.com>
2026-01-10 10:05:30 -0500

    Initial Commit

---
+++ Collectly/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Collectly
+//
+//  Created by Eric Chandonnet on 2026-01-10.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ CollectlyTests/CollectlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  CollectlyTests.swift
+//  CollectlyTests
+//
+//  Created by Eric Chandonnet on 2026-01-10.
+//
+
+import Testing
+@testable import Collectly
+
+struct CollectlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
